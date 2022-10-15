9bc5d240b5c75cd935be0b87982a9c00cb3cd68b
Vlad <dicusara9521@outlook.com>
2022-10-15 18:14:38 +0300

    footer

---
+++ src/components/navs/Footer.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { Col, Container, Icon, Row } from "sveltestrap";
+</script>
+
+<Container>
+  <Row class="border-top py-3">
+    <Col md="6"><Icon name="bootstrap-fill" /><span class="ms-3">© 2022 Company, Inc</span></Col>
+    <Col md="6" class="d-flex justify-content-end">Svelte, SvelteStrap</Col>
+  </Row>
+</Container>
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -4,13 +4,17 @@
   import { Col, Container, Row } from 'sveltestrap';
   import NavBar from '../components/navs/NavBar.svelte'
   import Menu from '../components/navs/Menu.svelte';
+  import Footer from '../components/navs/Footer.svelte';
 </script>
 
 <NavBar/>
-<Container fluid class="px-4">
-  <Row class="gx-5">
-    <Col md="3"><Menu/></Col>
-    <Col><slot></slot></Col>
-    <Col md="3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, eius. Ratione repudiandae mollitia nobis, porro voluptates perspiciatis alias necessitatibus, earum natus ex voluptate! Enim, architecto unde. Facilis iste accusantium id.</Col>
+<Container fluid>
+  <Row>
+    <Col md="3" class="py-5"><Menu/></Col>
+    <Col class="py-5"><Container><slot></slot></Container></Col>
+    <Col md="3" class="py-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, eius. Ratione repudiandae mollitia nobis, porro voluptates perspiciatis alias necessitatibus, earum natus ex voluptate! Enim, architecto unde. Facilis iste accusantium id.</Col>
+  </Row>
+  <Row>
+    <Col><Footer/></Col>
   </Row>
 </Container>
