02937ba90f69b12d02a01d9e9ef2433b5be7c829
trytofocus <dicusara9521@outlook.com>
2022-10-16 21:57:11 +0300

    base url

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,14 @@
   import { Styles } from 'sveltestrap'
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes.js";
+
+  const config = {
+    urlTransform: {
+      apply: url => `/Practice.Frontend.Svelte${url}`,
+      remove: url => url.replace("/Practice.Frontend.Svelte", "")
+    }
+  }
 </script>
 
 <Styles/>
-<Router {routes} />
+<Router {routes} {config} />
