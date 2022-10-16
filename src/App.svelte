5744cb73f9db83428e48ae4417a219c1e52e452a
trytofocus <dicusara9521@outlook.com>
2022-10-16 22:09:00 +0300

    revert router settings

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,7 @@
   import { Styles } from 'sveltestrap'
   import { Router } from "@roxi/routify";
   import { routes } from "../.routify/routes.js";
-
-  const config = {
-    urlTransform: {
-      apply: url => `/Practice.Frontend.Svelte${url}`,
-      remove: url => url.replace("/Practice.Frontend.Svelte", "")
-    }
-  }
 </script>
 
 <Styles/>
-<Router {routes} {config} />
+<Router {routes} />
